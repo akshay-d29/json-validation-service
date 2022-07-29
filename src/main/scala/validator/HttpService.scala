1e6ad0dfe67a6a0dc52bb4a873b6f6579404ea5e
Akshay Deshpande <akshay.deshpande@riskident.com>
2022-07-30 00:51:10 +0200

    handle invalid json

--- src/main/scala/validator/HttpService.scala
+++ src/main/scala/validator/HttpService.scala
@@ -5,9 +5,10 @@ import io.circe.Json
 import org.http4s.{HttpApp, HttpRoutes}
 import org.http4s.dsl.Http4sDsl
 import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
+
 import scala.language.postfixOps
 import io.circe.syntax.EncoderOps
-import validator.Domain.{Success, ValidationResponse}
+import validator.Domain.{Error, Success, UploadSchema, ValidationResponse}
 
 class HttpService(jsonRequestHandler: JsonRequestHandler) {
 
@@ -22,12 +23,12 @@ class HttpService(jsonRequestHandler: JsonRequestHandler) {
       .of[IO] {
         case request @ POST -> Root / `schema` / id =>
           (for {
-            requestJson <- request.as[Json].handleError(errorHandler)
+            requestJson <- request.as[Json]
             response    <- jsonRequestHandler.uploadSchema(requestJson, id)
           } yield response match {
             case res @ ValidationResponse(_, _, status) if status == Success => Created(res.asJson)
             case res                                                         => Ok(res.asJson)
-          }).flatten
+          }).flatten.handleErrorWith(ex => Ok(ValidationResponse(UploadSchema, id, Error(ex.getMessage)).asJson))
 
         case GET -> Root / `schema` / id =>
           jsonRequestHandler.getSchema(id).flatMap {
